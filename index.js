8b5dbe19a1c0e648c50267e38e497adb0e0a0b18
mohdkamleen <kamleenmohd@gmail.com>
2026-05-10 19:36:25 +0530

    demo

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const dotenv = require("dotenv");
 dotenv.config({ override: true, quiet: true });
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-try {
-  mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
-} catch (err) {
-  console.error(err);
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Mongo Error:", err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
@@ -23,6 +29,8 @@ app.get("/", (req, res) => {
   res.send("Server running....");
 });
 
-var port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+);
