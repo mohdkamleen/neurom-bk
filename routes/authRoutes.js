499373741776930a50e279db62b7cf286f389406
mohdkamleen <kamleenmohd@gmail.com>
2026-05-10 16:50:33 +0530

    ghbjnk

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 const {
   signup,
@@ -8,8 +9,25 @@ const {
   getProfile,
   deleteAllUsers
 } = require("../controllers/authController");
+ 
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-const authMiddleware = require("../middleware/authMiddleware");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded; // { id, email }
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
 
 // Routes
 router.post("/signup", signup);
