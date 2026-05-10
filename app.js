39ad5303685a58d240e6e3efacf46943db7739d2
mohdkamleen <kamleenmohd@gmail.com>
2026-05-10 13:33:41 +0530

    adsfs f

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));  
+//app.use("/api/auth", require("./routes/authRoutes"));  
 
 app.get("/", (req, res) => {
   res.send("Server running....");
