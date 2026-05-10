5a8a725d76c5893a81182a4bfa6be5bb42b43e7c
mohdkamleen <kamleenmohd@gmail.com>
2026-05-10 19:42:02 +0530

    fyguhijok

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 // config/db.js
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectDB = _ => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err); 
