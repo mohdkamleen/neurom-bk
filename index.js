37a2f348af16966a7a090d87cc01fa38370a8907
mohdkamleen <kamleenmohd@gmail.com>
2026-05-10 19:40:42 +0530

    tgyhujikl

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const dotenv = require("dotenv");
-dotenv.config({ override: true, quiet: true });
-
+dotenv.config({ override: true, quiet: true  });
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("Mongo Error:", err.message);
-    process.exit(1);
-  }
-};
+const connectDB = require("./config/db");
 
 connectDB();
 
-app.use("/api/auth", require("./routes/authRoutes"));
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));  
 
 app.get("/", (req, res) => {
   res.send("Server running....");
 });
+ 
+var port = process.env.PORT || 5000
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () =>
-  console.log(`Server running on port ${port}`)
-);
+app.listen(port, () => console.log(`Server running on port ${port}`));
